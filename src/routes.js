3c43a2eb86e53d88b9ef7a46bfd431543625d91a
BiancaBepple <bianca27513102@gmail.com>
2020-01-04 12:07:06 -0300

    Authentication.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
+import AuthController from './app/controllers/AuthController';
 
 const routes = Router();
 
 routes.get('/', (req, res) => res.json('JWT-NODE'));
+
+routes.post('/users', UserController.store);
+
+routes.post('/login', AuthController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/testeauth', (req, res) =>
+  res.json({ user: req.userId, result: 'Authenticated' })
+);
 
 export default routes;
