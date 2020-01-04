5891195a023226331463fe0457adbf15f7b87d1b
BiancaBepple <bianca27513102@gmail.com>
2020-01-04 09:45:03 -0300

    Adicionando migrations e sequelize model.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json('START-API-NODE'));
+routes.get('/', (req, res) => res.json('JWT-NODE'));
 
 export default routes;
